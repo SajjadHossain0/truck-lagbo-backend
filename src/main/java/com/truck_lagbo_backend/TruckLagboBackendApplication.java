2410661604f297b8393533d75be1e12e832c242d
mdsaj <mdsajjadhossain614@gmail.com>
2025-01-13 01:00:54 +0600

    new commit

---
+++ src/main/java/com/truck_lagbo_backend/TruckLagboBackendApplication.java
@@ -0,0 +1,13 @@
+package com.truck_lagbo_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TruckLagboBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TruckLagboBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/truck_lagbo_backend/TruckLagboBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.truck_lagbo_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TruckLagboBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
